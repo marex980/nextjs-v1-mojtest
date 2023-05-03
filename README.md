f376b98cec177d19d68d650edf1300e8abc973ef
marex980 <69250892+marex980@users.noreply.github.com>
2023-05-03 15:57:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-v1-mojtest
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-4f1hyz)
---
+++ pages/posts/first-post.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+function prviPost() {
+  return (
+    <>
+      <h1>Prvi post</h1>
+      <h2>
+        <Link href="/">Nazad</Link>
+      </h2>
+    </>
+  );
+}
+
+export default prviPost;
